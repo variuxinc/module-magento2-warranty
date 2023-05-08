44e943a333add6f18748c6e5b7cb6fa41ceca6a6
tung nguyen <tung.nguyen@beehexa.com>
2023-05-08 15:42:39 +0700

    engine registration info index page

--- Controller/Autosuggest/Engine.php
+++ Controller/Autosuggest/Engine.php
@@ -54,7 +54,7 @@ class Engine extends \Variux\Warranty\Controller\AbstractAction
          *       Sample: $this->resultFactory->create(ResultFactory::TYPE_JSON);
          */
         $resultJson = $this->resultFactory->create(ResultFactory::TYPE_JSON);
-        $resultJson->setData(json_encode($response));
+        $resultJson->setData($response);
         return $resultJson;
     }
 }
---
+++ Controller/Index/Engineinfo.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Variux\Warranty\Controller\Index;
+
+use Magento\Company\Model\CompanyContext;
+use Magento\Customer\Model\Session;
+use Variux\Warranty\Helper\SuggestHelper;
+
+class Engineinfo extends \Variux\Warranty\Controller\AbstractAction
+{
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        CompanyContext $companyContext,
+        \Psr\Log\LoggerInterface $logger,
+        Session $_customerSession,
+        \Variux\Warranty\Helper\Data $helperData,
+        SuggestHelper $suggestHelper
+    )
+    {
+        parent::__construct(
+            $context,
+            $companyContext,
+            $logger,
+            $_customerSession,
+            $helperData,
+            $suggestHelper
+        );
+    }
+
+    public function execute()
+    {
+        $this->_view->loadLayout();
+        $this->_view->getPage()->getConfig()->getTitle()->set(__('Engine Registration Information'));
+        $this->_view->renderLayout();
+    }
+}
