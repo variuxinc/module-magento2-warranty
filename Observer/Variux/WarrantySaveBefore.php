b6fd4ed94b6986baa2b7c436664ce3b83ebe7706
tung nguyen <tung.nguyen@beehexa.com>
2023-06-26 08:51:51 +0700

    email notify implementation

---
+++ Observer/Variux/WarrantySaveBefore.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Variux\Warranty\Observer\Variux;
+
+use Magento\Framework\Event\Observer;
+use Variux\Warranty\Logger\Logger;
+use Variux\Warranty\Model\Email\Warranty\Sender as WarrantyEmailSender;
+use Magento\Customer\Api\CustomerRepositoryInterface as CustomerRepository;
+use Variux\Warranty\Helper\Config as Config;
+
+class WarrantySaveBefore implements \Magento\Framework\Event\ObserverInterface
+{
+    /**
+     * @var Logger
+     */
+    protected $logger;
+    /**
+     * @var WarrantyEmailSender
+     */
+    protected $warrantyEmailSender;
+    /**
+     * @var CustomerRepository
+     */
+    protected $customerRepository;
+
+    /**
+     * @param Logger $logger
+     * @param WarrantyEmailSender $warrantyEmailSender
+     * @param CustomerRepository $customerRepository
+     */
+    public function __construct(
+        Logger $logger,
+        WarrantyEmailSender $warrantyEmailSender,
+        CustomerRepository $customerRepository,
+        Config $config
+    ) {
+        $this->logger = $logger;
+        $this->warrantyEmailSender = $warrantyEmailSender;
+        $this->customerRepository = $customerRepository;
+        $this->config = $config;
+    }
+
+    public function execute(Observer $observer)
+    {
+        $warranty = $observer->getEvent()->getDataObject();
+        if ($warranty->getId()) {
+            try {
+                $this->logger->info("Warranty status updated" . $warranty->getStatus());
+                $emailTo = $this->customerRepository->getById($warranty->getCustomerId())->getEmail();
+                $this->logger->info("email To: ". $emailTo);
+                $senderEmail = $this->config->getEmailIdentity();
+                $storeId = 1;
+                $template = 'claim_status_change_template';
+                $warrantyId = $warranty->getId();
+                $this->logger->info("warrantyId: ". $warrantyId);
+                $customerName = $this->customerRepository->getById($warranty->getCustomerId())->getLastname();
+                $this->logger->info("Csutomer name: ". $customerName);
+                $status = $warranty->getStatus();
+                $this->warrantyEmailSender->sendMail($emailTo, $senderEmail, $storeId, $template, $warrantyId, $customerName, $status);
+                $this->logger->info('Hello');
+            }
+            catch (\Exception $e) {
+                $this->logger->critical($e);
+            }
+        }
+    }
+}
